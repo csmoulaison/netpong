895c570f9797b2ffb4932c03e53cb2a5719ecf41
Conner Moulaison <csmoulaison@gmail.com>
2025-11-19 23:40:31 -0800

    Fix text opacity blenidng

--- src/renderer/opengl/opengl.cpp
+++ src/renderer/opengl/opengl.cpp
@@ -100,6 +100,7 @@ Render::Context* platform_render_init(Windowing::Context* window, Arena* arena)
 
 	glEnable(GL_DEPTH_TEST);
 	glEnable(GL_BLEND);
+	glDepthFunc(GL_LEQUAL);
 	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
 	// Quad rendering
